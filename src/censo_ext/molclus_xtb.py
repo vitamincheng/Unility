2eb712e20625aff6ac0b8ad00ec48ca0461bcfe8
Vitamin Cheng <vitmain.cheng@gmail.com>
2025-08-06 16:37:47 +0800

    fixed bug of molclus_xtb.py about print f function

--- src/censo_ext/molclus_xtb.py
+++ src/censo_ext/molclus_xtb.py
@@ -117,7 +117,7 @@ def main(args: argparse.Namespace = argparse.Namespace()) -> None:
     if args == argparse.Namespace():
         args = cml(descr)
 
-    single_traj_Name = ".single_traj.xyz"
+    single_traj_Name = ".traj.xyz"
     temp_isomer_Name = ".isomers_tmp.xyz"
     xtb_cmd: str = ""
     infile: GeometryXYZs = GeometryXYZs(args.file)
@@ -160,11 +160,11 @@ def main(args: argparse.Namespace = argparse.Namespace()) -> None:
         # idx_str : str = f"{[idx]:05d}"
         infile.set_filename(Path(single_traj_Name))
         infile.method_save_xyz([idx])
-        print("                          *** Configuration         {str(idx)}  ****")  # nopep8
-        print(" Loading geometry	 {str(idx)}  from the inputted geometry file")      # nopep8
+        print(f"                          *** Configuration         {str(idx)}  ****")  # nopep8
+        print(f" Loading geometry	 {str(idx)}  from the inputted geometry file")      # nopep8
         print(" Generating  file...")
         subprocess.call(xtb_cmd, shell=True)
-        print(" Running:  {xtb_cmd}")
+        print(f" Running:  {xtb_cmd}")
 
         get_energy: int | None = None
         if args.opt:
