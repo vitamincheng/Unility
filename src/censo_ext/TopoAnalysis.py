6f8d630a29fa2576f83a6a0db44d5b663dfdb752
Vitamin Cheng <vitmain.cheng@gmail.com>
2025-10-21 16:59:15 +0800

    ✨ feat(TopoAnalysis.py):
    create one to topo analysis to test

---
+++ src/censo_ext/TopoAnalysis.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+import argparse
+from censo_ext.Tools.utility import print_arguments
+from censo_ext.Tools.xyzfile import GeometryXYZs
+from censo_ext.xyzGenFlexible import get_xyzSplit, read_data
+descr = """
+________________________________________________________________________________
+| Input   : -i input.xyz 
+|______________________________________________________________________________
+"""
+
+
+def cml() -> argparse.Namespace:
+    """ Get args object from commandline interface.
+        Needs argparse module."""
+    parser = argparse.ArgumentParser(
+        description=f"{descr}",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        usage=argparse.SUPPRESS,
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest="file",
+        action="store",
+        required=False,
+        type=str,
+        help="Provide two input_file name ",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--idx",
+        dest="idx",
+        action="store",
+        required=False,
+        type=int,
+        default=1,
+        help="Provide index of xyz file [default 1] ",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--limits",
+        dest="limits",
+        action="store",
+        required=False,
+        type=float,
+        default=0.4,
+        help="Provide limits of delta std of xyz files [default 0.4] ",
+    )
+
+    args: argparse.Namespace = parser.parse_args()
+    return args
+
+
+def cal_RMSD(xyzfile, idx_p, idx_q, bond_broken) -> float:
+    from censo_ext.Tools.calculate_rmsd import cal_RMSD_xyz
+    x: dict = {"remove_idx": None, "add_idx": None,
+               "bond_broken": bond_broken, "ignore_Hydrogen": True}
+    _, RMSD = cal_RMSD_xyz(
+        xyzfile, idx_p, idx_q, args=argparse.Namespace(**x))
+    return RMSD
+
+
+def TopoAnalysis(args) -> None:
+
+    idx_p = args.idx
+    args.verbose = False
+    neighbor, circleMols, residualMols, Bond_order, atomsCN, residualMols_all_pairs = read_data(
+        args)
+    flattenCircleMols: list[int] = []
+    for mol in circleMols:
+        flattenCircleMols += mol
+    flattenCircleMols = list(set(flattenCircleMols))
+
+    xyzSplit: dict[int, int] = get_xyzSplit(residualMols,
+                                            Bond_order, atomsCN, flattenCircleMols, residualMols_all_pairs)
+    from icecream import ic
+    ic(residualMols)
+    ic(circleMols)
+    ic(neighbor)
+    ic(flattenCircleMols)
+    # ic(residualMols_all_pairs)
+    xyzFile: GeometryXYZs = GeometryXYZs(args.file)
+    xyzFile.method_read_xyz()
+    limits = args.limits
+
+    # Check circleMols factor
+    for resMol in residualMols:
+        # ic(resMol)
+        node_mols = [x for x in resMol if x in flattenCircleMols]
+        for node_mol in node_mols:
+            # ic(node_mol)
+            x = set.intersection(
+                set(map(int, neighbor[int(node_mol)])), resMol)
+            if (len(x)) != 1:
+                print("  Something wrong in your residualMols")
+                print("  Exit and Close the program !!!")
+                exit(1)
+            res_node_mol = list(x)[0]
+            # ic(node_mol, res_node_mol)
+            nNums = len(xyzFile)
+            for x in range(1, nNums+1):
+                if x == idx_p:
+                    continue
+                res_left = cal_RMSD(xyzfile=xyzFile, idx_p=idx_p, idx_q=x,
+                                    bond_broken=(node_mol, res_node_mol))
+                res_right = cal_RMSD(xyzfile=xyzFile, idx_p=idx_p, idx_q=x,
+                                     bond_broken=(res_node_mol, node_mol))
+                if res_left <= limits and res_right <= limits:
+                    ic(x, node_mol, res_node_mol, res_left, res_right)
+
+    # Check straight chain
+    for key, value in xyzSplit.items():
+        nNums = len(xyzFile)
+        for x in range(1, nNums+1):
+            if x == idx_p:
+                continue
+            res_left = cal_RMSD(xyzfile=xyzFile, idx_p=idx_p, idx_q=x,
+                                bond_broken=(key, value))
+            res_right = cal_RMSD(xyzfile=xyzFile, idx_p=idx_p, idx_q=x,
+                                 bond_broken=(value, key))
+            if res_left <= limits and res_right <= limits:
+                ic(x, key, value, res_left, res_right)
+
+
+def main(args: argparse.Namespace = argparse.Namespace()) -> None:
+    if args == argparse.Namespace():
+        args = cml()
+    print_arguments()
+
+    TopoAnalysis(args)
+
+
+if __name__ == "__main__":
+    main()
