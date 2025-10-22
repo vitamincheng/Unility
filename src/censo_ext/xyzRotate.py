33f4d5fe796bf0878130f357b0a6728417fae1ec
Vitamin Cheng <vitmain.cheng@gmail.com>
2025-10-22 09:55:18 +0800

    ✨ feat(xyzRotate.py): specific rotation
    specific rotation of xyz file

---
+++ src/censo_ext/xyzRotate.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+from scipy.spatial.transform import Rotation as R
+import argparse
+from pathlib import Path
+import numpy as np
+import numpy.typing as npt
+from censo_ext.Tools.utility import print_arguments
+from censo_ext.Tools.xyzfile import GeometryXYZs
+descr = """
+________________________________________________________________________________
+| For rotation the confomrer from specific angle of cleavage specifying two atoms                        
+| Usage    : xyzRotate.py [options]                  
+| Input    : -i Read xyz file [default traj.xyz]
+| Output   : -o Save xyz file [default output.xyz] 
+| [Options]
+| Atom     : -a or --atom [1 2] idx of atom's number  
+|              1 : Fixed atom
+|              2 : Rotation axis atom (360 degrees) 
+| nCut     : -c or cut Number of cut to make 360 degrees around the roation axis 
+| sPecific : -s Specific number of the cut's number from 1 to (nCuts-1) 
+| Print    : -p Print output to screen [default False]
+|______________________________________________________________________________
+"""
+
+
+def cml() -> argparse.Namespace:
+    """ Get args object from commandline interface.
+        Needs argparse module."""
+    parser = argparse.ArgumentParser(
+        description=f"{descr}",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        usage=argparse.SUPPRESS,
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest="file",
+        action="store",
+        required=False,
+        default="traj.xyz",
+        help="Provide one input xyz file [default traj.xyz]",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="out",
+        action="store",
+        required=False,
+        default="output.xyz",
+        help="Provide one output xyz file [defalut output.xyz]",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--atom",
+        dest="atoms",
+        action="store",
+        type=int,
+        nargs=2,
+        default=None,
+        required=True,
+        metavar=('FIXED', 'ROTATION'),
+        help="two atom indics: first is fixed, second is rotation axis"
+    )
+
+    parser.add_argument(
+        "-c",
+        "--cut",
+        dest="cuts",
+        action="store",
+        type=int,
+        default=3,
+        required=False,
+        help="Number of cuts to make in 360 degrees around the rotation axis [default 3]",
+    )
+    parser.add_argument(
+        "-s",
+        "--specific",
+        dest="spec",
+        action="store",
+        type=int,
+        default=1,
+        required=False,
+        help="Specific number of cut number the rotation axis [default 1]",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--print",
+        dest="print",
+        action="store_true",
+        help="Print output to screen [default False]",
+    )
+    parser.add_argument(
+        "--check",
+        dest="check",
+        action="store_true",
+        help="Check hydrogen atom is only one bond [default False]",
+    )
+
+    args: argparse.Namespace = parser.parse_args()
+    return args
+
+
+def main(args: argparse.Namespace = argparse.Namespace()) -> None:
+
+    if args == argparse.Namespace():
+        args = cml()
+    print_arguments()
+
+    from censo_ext.Tools.utility import IsExist
+    inFile = Path(args.file)
+    outFile = Path(args.out)
+    IsExist(inFile)
+
+    if not args.cuts or not args.atoms:
+        print("  Please input your atoms that you want to split ")
+        print("  Exit and Close the program !!!")
+        exit(0)
+
+    from censo_ext.Tools.utility import delete_all_files
+    if not args.print:
+        delete_all_files(outFile)
+
+    idx1_p, idx1_q = args.atoms
+    nCutters: int = args.cuts
+    nSpec = args.spec
+    if nSpec >= nCutters or nSpec <= 0:
+        print("  Specific number of cut' number is error !!!")
+        print("  Exit and Close the program !!!")
+        exit(1)
+
+    x: dict = {"file": inFile, "bond_broken": [idx1_q, idx1_p],
+               "print": False, "debug": False}
+
+    from censo_ext.Tools.topo import Topo
+    Sts_topo: Topo = Topo(x["file"], check=args.check)
+    idx0_broken_bond_H: list[int] = [
+        x-1 for x in Sts_topo.method_broken_bond_H(argparse.Namespace(**x))]
+
+    xyzFile: GeometryXYZs = GeometryXYZs(inFile)
+    xyzFile.method_read_xyz()
+
+    for idx1_St, St in enumerate(xyzFile.Sts, 1):
+
+        dxyz: npt.NDArray[np.float64] = St.coord[idx1_p-1].copy()
+        inital: list[npt.NDArray[np.float64]] = St.coord.copy()
+
+        for nCutter in range(nCutters):
+
+            St.coord = inital.copy()
+            St.coord -= dxyz  # type: ignore
+
+            rotation_axis: npt.NDArray[np.float64] = St.coord[idx1_q-1]
+            rotation_vector: npt.NDArray[np.float64] = rotation_axis / \
+                np.linalg.norm(rotation_axis)
+
+            r_pq = R.from_rotvec(2*np.pi*(nCutter/nCutters)*rotation_vector)
+
+            for idx0 in idx0_broken_bond_H:
+                St.coord[idx0] = r_pq.apply(St.coord[idx0])
+
+            St.coord += dxyz
+
+            if nCutter == nSpec:
+                if args.print:
+                    xyzFile.method_print([idx1_St])
+                else:
+                    xyzFile.set_filename(outFile)
+                    xyzFile.method_save_xyz_append([idx1_St])
+
+    if not args.print:
+        print(f"    Save to the file : {outFile}")
+
+
+if __name__ == "__main__":
+    main()
+
+    # For example
+    #   xyzSplit.py -i tests/data/crest_conformers1.xyz -a 52 55 -c 3
+    #   xyzSplit.py -i tests/data/crest_conformers1.xyz -a 52 55 -c 3 -p
