88eff59862e106982a4ab75ebc4d21e0e105f0ae
Vitamin Cheng <vitmain.cheng@gmail.com>
2025-10-29 16:16:42 +0800

    🦄 refactor(TopoAnalysis.py): refactor
    opt for print format

--- src/censo_ext/TopoAnalysis.py
+++ src/censo_ext/TopoAnalysis.py
@@ -9,7 +9,7 @@ ________________________________________________________________________________
 | Input    : -i input xyz file [default traj.xyz]
 | [options]
 | index     : -d index of reference structure in xyz file [defalut 1]
-| limits    : -l limits of delta std in xyz file [default 0.40]
+| limits    : -l limits of delta std in xyz file [default 0.10]
 |______________________________________________________________________________
 """
 
@@ -52,8 +52,8 @@ def cml() -> argparse.Namespace:
         action="store",
         required=False,
         type=float,
-        default=0.4,
-        help="Provide limits of delta std in xyz file [default 0.4] ",
+        default=0.1,
+        help="Provide limits of delta std in xyz file [default 0.1] ",
     )
 
     args: argparse.Namespace = parser.parse_args()
@@ -91,8 +91,12 @@ def TopoAnalysis(args) -> None:
     xyzFile: GeometryXYZs = GeometryXYZs(args.file)
     xyzFile.method_read_xyz()
     limits = args.limits
+    print("  ===== Parameter of limits =====")
+    print(f"  the delta limits of standard deviation = {limits}")
 
     # Check circleMols factor
+    print("  ===== Check circle molecule =====")
+    result_circle: list = []
     for resMol in residualMols:
         # ic(resMol)
         node_mols = [x for x in resMol if x in flattenCircleMols]
@@ -115,9 +119,14 @@ def TopoAnalysis(args) -> None:
                 res_right = cal_RMSD(xyzfile=xyzFile, idx_p=idx_p, idx_q=x,
                                      bond_broken=(res_node_mol, node_mol))
                 if res_left <= limits and res_right <= limits:
-                    ic(x, node_mol, res_node_mol, res_left, res_right)
-
+                    result_circle.append(
+                        (x, node_mol, res_node_mol, res_left, res_right))
+    print("   idx1  node  res_node      res_left      res_right")
+    for x in result_circle:
+        print(f"{x[0]:6d} {x[1]:6d} {x[2]:8d} {x[3]:14.7f} {x[4]:14.7f}")
     # Check straight chain
+    print("  ===== Check straight molecule =====")
+    result_straight: list = []
     for key, value in xyzSplit.items():
         nNums = len(xyzFile)
         for x in range(1, nNums+1):
@@ -128,7 +137,11 @@ def TopoAnalysis(args) -> None:
             res_right = cal_RMSD(xyzfile=xyzFile, idx_p=idx_p, idx_q=x,
                                  bond_broken=(value, key))
             if res_left <= limits and res_right <= limits:
-                ic(x, key, value, res_left, res_right)
+                # ic(x, key, value, res_left, res_right)
+                result_straight.append((x, key, value, res_left, res_right))
+    print("   idx1   key     value      res_left      res_right")
+    for x in result_straight:
+        print(f"{x[0]:6d} {x[1]:6d} {x[2]:8d} {x[3]:14.7f} {x[4]:14.7f}")
 
 
 def main(args: argparse.Namespace = argparse.Namespace()) -> None:
