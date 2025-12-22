6909ee5e683c0ee941c25c232fe5dcf57164afa3
Vitamin Cheng <vitmain.cheng@gmail.com>
2025-12-22 13:42:04 +0800

    🦄 refactor(xyzDuplicate.py): refactor

--- src/censo_ext/xyzDuplicate.py
+++ src/censo_ext/xyzDuplicate.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 from pathlib import Path
-from censo_ext import xyzReturnOandZ
+# from censo_ext import xyzReturnOandZ
 from censo_ext.Tools.calculate_rmsd import cal_RMSD_xyz
 from censo_ext.Tools.utility import IsExists_DirFileName, print_arguments
 import argparse
 from censo_ext.Tools.xyzfile import GeometryXYZs
 descr = """
     ________________________________________________________________________________
-    | For Generation of xyz molecule
-    | Usages   : xyz.py <geometry> [options]
+    | For remove Duplicate confomrers by RMSD 
+    | Usages   : xyzDuplicate.py <geometry> [options]
     | [options]
     |______________________________________________________________________________
     """
@@ -51,36 +51,40 @@ def cml() -> argparse.Namespace:
     return args
 
 
+def Duplicate_process(_rthr: float, _xyzFile: GeometryXYZs):
+    print(
+        f"\n  The numbers of the structures in xyz files : {len(_xyzFile.Sts)}")
+
+    idx1_Sts: list[int] = [x+1 for x in [*range(len(_xyzFile.Sts))]]
+
+    for idx1_p in range(1, len(_xyzFile.Sts)+1):
+        if len(idx1_Sts) >= 1:
+            for idx1_q in idx1_Sts.copy():
+                if idx1_p != idx1_q and idx1_p < idx1_q:
+                    _, result_RMSD = cal_RMSD_xyz(xyzFile=_xyzFile, idx1_p=idx1_p,
+                                                  idx1_q=idx1_q, _add_idx=None, _remove_idx=None, _bond_broken=None, _ignore_Hydrogen=True)
+                    if result_RMSD <= _rthr:
+                        idx1_Sts.remove(idx1_q)
+        else:
+            pass
+
+    _xyzFile.method_xyzExtract([x-1 for x in idx1_Sts])
+
+
 def main(args: argparse.Namespace = argparse.Namespace()) -> None:
     if args == argparse.Namespace():
         args = cml()
     print_arguments()
 
-    moment: dict = {"file": args.file, "auto": True,
-                    "atom": None, "print": False, "replace": True, "out": None}
-    x_args = argparse.Namespace(**moment)
-    xyzReturnOandZ.main(x_args)
+    # moment: dict = {"file": args.file, "auto": True,
+    #                "atom": None, "print": False, "replace": True, "out": None}
+    # x_args = argparse.Namespace(**moment)
+    # xyzReturnOandZ.main(x_args)
 
     xyzFile: GeometryXYZs = GeometryXYZs(args.file)
     xyzFile.method_read_xyz()
 
-    print(
-        f"\n  The numbers of the structures in xyz files : {len(xyzFile.Sts)}")
-
-    idx1_Sts: list[int] = [x+1 for x in [*range(len(xyzFile.Sts))]]
-
-    for idx1_p in range(1, len(xyzFile.Sts)+1):
-        if len(idx1_Sts) >= 1:
-            for idx1_q in idx1_Sts.copy():
-                if idx1_p != idx1_q and idx1_p < idx1_q:
-                    _, result_RMSD = cal_RMSD_xyz(xyzFile=xyzFile, idx1_p=idx1_p,
-                                                  idx1_q=idx1_q, _add_idx=None, _remove_idx=None, _bond_broken=None, _ignore_Hydrogen=True)
-                    if result_RMSD <= args.rthr:
-                        idx1_Sts.remove(idx1_q)
-        else:
-            pass
-
-    xyzFile.method_xyzExtract([x-1 for x in idx1_Sts])
+    Duplicate_process(_rthr=args.rthr, _xyzFile=xyzFile)
 
     Dir, file = IsExists_DirFileName(Path(args.file))
     file_split: list[str] = file.split(".")
