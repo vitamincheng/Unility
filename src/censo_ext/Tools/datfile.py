d68cc1681eb2aea89640d51a0aa3d54c8538a7ae
Vitamin Cheng <vitmain.cheng@gmail.com>
2025-12-31 14:58:25 +0800

    ✨ feat(datManipulate.py and datfile.py): add intensit
    add intensity parameter for datfile and update the datfile library

--- src/censo_ext/Tools/datfile.py
+++ src/censo_ext/Tools/datfile.py
@@ -621,3 +621,12 @@ class CensoDat():
             npt.NDArray: The data array.
         """
         self.__dat.T[0] = self.__dat.T[0] + ppm_shift
+
+    def Intensit_Dat(self, Intensit: float) -> None:
+        """
+        Get the raw data array.
+
+        Returns:
+            npt.NDArray: The data array.
+        """
+        self.__dat.T[1] = self.__dat.T[1] * Intensit
--- src/censo_ext/datManipulate.py
+++ src/censo_ext/datManipulate.py
@@ -36,7 +36,6 @@ def cml() -> argparse.Namespace:
         help="Provide output dat file name [default output.dat]",
     )
     parser.add_argument(
-        "-s",
         "--shift",
         dest="shift",
         action="store",
@@ -44,6 +43,15 @@ def cml() -> argparse.Namespace:
         type=float,
         default=0.0,
         help="Provide the shift ppm [default 0.0]",
+    )
+    parser.add_argument(
+        "--intensit",
+        dest="intensit",
+        action="store",
+        required=False,
+        type=float,
+        default=1.0,
+        help="Provide the shift ppm [default 1.0]",
     )
     args: argparse.Namespace = parser.parse_args()
     return args
@@ -62,6 +70,7 @@ def main(args: argparse.Namespace = argparse.Namespace()) -> None:
         from censo_ext.Tools.datfile import CensoDat
         inDat: CensoDat = CensoDat(inFile)
         inDat.ppm_shift_Dat(ppm_shift=args.shift)
+        inDat.Intensit_Dat(Intensit=args.intensit)
         inDat.set_fileName(outFile)
         inDat.method_save_dat()
 
